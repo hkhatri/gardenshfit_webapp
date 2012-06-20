ba21f0533a657aa24dfee0c46daf6cba1f46dbc9
Hilay Khatri <hilaykhatri@localhost.localdomain>
2012-06-20 10:47:38 -0400

    welcome page

--- php/application/views/templates/header.php
+++ php/application/views/templates/header.php
@@ -1,7 +1,8 @@
 <html>
+
 <head>
-	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+	<title>Welcome to Gardenshift</title>
+	<link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
 <body>
-	<h1>CodeIgniter 2 Tutorial</h1>
 	
