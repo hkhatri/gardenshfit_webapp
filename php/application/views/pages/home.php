4d389b6662d5c4c4de3331deef4fc5ec52b81ed8
Hilay Khatri <hilaykhatri@dhcp226-226.rdu2.redhat.com>
2012-07-12 15:42:13 -0400

    profile picture + friends + feeback + gmaps + all crops

--- php/application/views/pages/home.php
+++ php/application/views/pages/home.php
@@ -39,22 +39,7 @@
 <script> 
     
 $(document).ready( function() {
-    
-    function submitenter(myfield,e)
-    {
-        var keycode;
-        if (window.event) keycode = window.event.keyCode;
-        else if (e) keycode = e.which;
-        else return true;
 
-        if (keycode == 13)
-        {
-        myfield.form.submit();
-        return false;
-        }
-        else
-        return true;
-    }
          
     init();
     
--- php/js/initialization.js
+++ php/js/initialization.js
@@ -70,5 +70,23 @@ var newUser = document.getElementById('newUser');
 newUser.onclick = showAddUser;
 
 
+function submitenter(myfield,e)
+    {
+        var keycode;
+        if (window.event) keycode = window.event.keyCode;
+        else if (e) keycode = e.which;
+        else return true;
+
+        if (keycode == 13)
+        {
+        alert("asfasf");
+        myfield.form.submit();
+        return false;
+        }
+        else
+        return true;
+    }
+
+
 
 }
